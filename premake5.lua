40dcd9173a01e81cff9656e554de8e6632923426
HEYHOY <31182022+duelas@users.noreply.github.com>
2025-02-22 05:58:24 +0800

    Add files via upload

---
+++ premake5.lua
@@ -0,0 +1,53 @@
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		
+		"include/**.h"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	postbuildcommands
+	{
+		"{COPYDIR} \"%{cfg.targetdir}/%{prj.name}.lib\" \"%{wks.location}/projectmodule/Lib\""
+	}
+	
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++20"
+		staticruntime "off"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++20"
+		staticruntime "off"
+		
+	filter "system:macos"
+		systemversion "latest"
+		cppdialect "C++20"
+		staticruntime "off"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
